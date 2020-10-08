5a6aaff926ae569d3ac2c49427bdc33a3d4b7df3
Craig Chamberlain <craig.chamberlain11@gmail.com>
2020-10-08 10:17:10 +0100

    Add babel and react locally

--- generator.js
+++ generator.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const target = document.getElementById('target')
 var array = new Array(4584).fill(undefined).map((_,index)=> {
     var style = (Math.random() < .4) ? {visibility:'hidden'} : {};
-    return <div key={index} style={style}/>
+    return (<div key={index} style={style}/>)
 
 });
 ReactDOM.render(array, target);
