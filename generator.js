4b076c4ee5776b872703ef4571461241e122f7f7
Craig Chamberlain <craig.chamberlain11@gmail.com>
2020-10-08 10:30:16 +0100

    Add form & default phases to invisible

--- generator.js
+++ generator.js
@@ -2,9 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const target = document.getElementById('target')
-var array = new Array(4584).fill(undefined).map((_,index)=> {
-    var style = (Math.random() < .4) ? {visibility:'hidden'} : {};
-    return (<div key={index} style={style}/>)
 
-});
-ReactDOM.render(array, target);
+function Phase({index, ...rest}){
+  const [visibility, setVisibility] = React.useState(UpdateVisibilityStyle());
+  const handleChange = () => setVisibility({visibility:'visible'});
+  //var style = visibility? {visibility:'visible'} : {};
+  return <div key={index} style={visibility} {...rest}/>
+}
+
+function CreatePhases(numberOfPhases){
+    const array = new Array(numberOfPhases).fill(undefined).map((_,index)=> <Phase key={index}/>);
+    return (
+      <div id="phases">
+        {array}
+      </div>
+    )
+}
+function UpdateVisibilityStyle(){
+  return (Math.random() > .3) ? {visibility:'visible'} : {};
+}
+
+
+function App(){
+  const [numberOfPhases, setnumberOfPhases] = React.useState(
+    parseInt(window.localStorage.getItem('numberOfPhases')) || 4584,
+    );
+  React.useEffect(() => {
+    window.localStorage.setItem('numberOfPhases', numberOfPhases)
+  })
+  const handleChange = event => setnumberOfPhases(parseInt(event.target.value));
+  return(
+            <div>
+              <form onSubmit={(e)=>e.preventDefault()}>
+                <label htmlFor="numberOfPhases">Number of Phases</label>
+                <input type="number" onChange={handleChange} id="numberOfPhases" value={numberOfPhases.toString()}/>
+              </form>
+              {CreatePhases(numberOfPhases)}
+            </div> 
+          
+  )
+  
+}
+ReactDOM.render(<App />, target);
